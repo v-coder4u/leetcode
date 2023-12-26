f4f35ebc5b6d1f5435636179e9586f037ee13db4
Vaibhav Varshney <v.varshney97@gmail.com>
2023-12-26 19:04:15 +0530

    Update Max_Consecutive_Ones_III.java

--- Max_Consecutive_Ones_III.java
+++ Max_Consecutive_Ones_III.java
@@ -18,3 +18,31 @@ class Solution {
         return max;
     }
 }
+
+
+// Related Questions 1:
+// Max Consecutive Ones
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0; 
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                count++;    
+            } else {
+                count = 0;
+            } 
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
+
+
+//Related Questions 2
+//Max Consecutive Ones 2
+
+//Given a binary array, find the maximum number of consecutive 1s in this array if you can flip at most one 0.
+
+//fix k = 1 with longestOnes problem.
